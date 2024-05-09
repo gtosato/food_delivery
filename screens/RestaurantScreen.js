1762418255aac62b31964ab2088cbafdf52bb9ff
Greg Tosato <gregtosato@gmail.com>
2024-05-10 02:52:28 +1000

    bug fix on back buttons

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -68,7 +68,7 @@ const RestaurantScreen = () => {
 						className="w-full h-56 p-4"
 					/>
 					<TouchableOpacity
-						onPress={() => navigation.goBack()}
+						onPress={navigation.goBack}
 						className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
 					>
 						<ArrowLeftIcon size={20} color="#00CCBB" />
